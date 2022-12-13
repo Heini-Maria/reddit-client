82f6c69519b6d53bce1fcbbb788a8df7c49abd4a
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-12-13 13:04:27 +0100

    tried dofferent approach with reddit data

--- src/App/App.js
+++ src/App/App.js
@@ -4,9 +4,26 @@ import Diy from '../Features/Diy/Diy';
 import Crafts from '../Features/Crafts/Crafts';
 import { Routes, Route } from 'react-router-dom';
 import Comments from '../Features/Comments/Comments';
+import React, {useState, useEffect} from 'react';
 
 
 function App() {
+const [articles, setArticles] = useState([]);
+const [subreddit, setSubreddit] = useState('diy');
+
+useEffect (() => {
+  fetch("https://www.reddit.com/r/diy.json").then (res => {
+    if (res.status != 200) {
+      console.log('ERROOR');
+      return;
+    }
+    res.json().then(data => {
+      if(data !=null) {
+        console.log(data);
+      }
+    })
+  })
+})
 
   return (
     <div className="App">
--- src/Components/Reddit.js
+++ src/Components/Reddit.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export const API_ROOT = 'https://www.reddit.com';
+export const API_ROOT = 'https://www.reddit.com/r/';
 
-export const getSubredditPosts = async (subreddit) => {
-    const response = await fetch('${API_ROOT}${subreddit}.json');
+export const getSubredditPosts = async (crafts) => {
+    const response = await fetch('${API_ROOT}${crafts}.json');
     const json = await response.json();
 
     return json.data.children.map((post) = post.data);
+
 };
+console.log(getSubredditPosts());
 
 export const getPostComments = async (permalink) => {
     const response = await fetch(`${API_ROOT}${permalink}.json`);
