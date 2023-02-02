6b581d8899d71f0c62d698e47e194b6c7b394a53
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-02-02 10:44:11 +0100

    cleaned comments.js

--- src/Features/Comments/Comments.js
+++ src/Features/Comments/Comments.js
@@ -1,39 +1,33 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import commentsicon from '../../Assets/Images/commentsicon.png';
 import close from '../../Assets/Images/close.png';
-import Comment from '../Comment/Comment.js';
-import { useDispatch } from 'react-redux';
+import Comment from '../Comment/Comment';
 import { toggleShowingComments } from '../Feed/FeedSlice';
 
+function Comments({ comments, index }) {
+  const dispatch = useDispatch();
+  const handleClick = async () => {
+    dispatch(toggleShowingComments(index));
+  };
 
-const Comments = ({ comments, index }) => {
-    const dispatch = useDispatch();
- 
-    const handleClick = async () => {
-        dispatch(toggleShowingComments(index))
-      };
-
-    
-console.log(comments);
-
-    return (
-    <section className='comments'>
-        <div className='comments-top-bar'>
-            <article className='top-bar-item'>
-                <img className='comments-icon'src={commentsicon} alt='comments icon'/>
-                <h2>{comments.length} Comments</h2>
-            </article>
-            <aside className='top-bar-item'>
-                <button onClick = {handleClick}>
-                    <img className='close-icon' src={close} />
-                </button>
-            </aside>
-        </div>
-            {
-    
-            comments.map((comment, index) => <Comment key={index} index={index} comment={comment} /> )
-            }    
-    </section>    
-    )
+  return (
+    <section className="comments">
+      <div className="comments-top-bar">
+        <article className="top-bar-item">
+          <img className="comments-icon" src={commentsicon} alt="comments icon" />
+          <h2>
+            {comments.length} Comments</h2>
+        </article>
+        <aside className="top-bar-item">
+          <button type="button" onClick={handleClick}>
+            <img className="close-icon" src={close} alt="close" />
+          </button>
+        </aside>
+      </div>
+      {comments.map((comment, index) => <Comment key={index} index={index} comment={comment} /> )}
+    </section>
+  );
 }
 
 export default Comments;
