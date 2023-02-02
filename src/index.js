414610b312b63ba3a09aff7a582f2d60615a92b3
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-02-02 11:24:38 +0100

    cleaned index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from '../src/App/store';
-import { Provider } from 'react-redux';
-
+import { store } from './App/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <Provider store={store} >
-        <App /> 
-    </Provider>    
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
