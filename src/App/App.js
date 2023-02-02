69bdf06925fa2c2bf1e21bafee1edd2867631302
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-02-02 10:18:02 +0100

    cleaned App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,21 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Header from '../Components/Header';
 import Sewing from '../Features/Sewing/Sewing';
 import Crafts from '../Features/Crafts/Crafts';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Comments from '../Features/Comments/Comments';
-import React from 'react';
-
 
 function App() {
-
-
   return (
     <div className="App">
       <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Sewing />}/>
-        <Route path='/crafts' element={<Crafts />} />
-       <Route path='/' element={<Comments />}/>
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Sewing />} />
+          <Route path="/crafts" element={<Crafts />} />
+          <Route path="/" element={<Comments />} />
+        </Routes>
       </Router>
     </div>
   );
