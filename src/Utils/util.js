bf30f3888a67a789544682f074d0710901f96520
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-02-04 15:28:18 +0100

    fixed CSS

--- src/Utils/util.js
+++ src/Utils/util.js
@@ -8,27 +8,24 @@ export const utcToString = (utcCreated) => {
   const secondsMonth = secondsDay * 30.437;
   const secondsYear = secondsDay * 365;
 
-
-    if ( secondsSince < secondsMin) {
-        return `less than minute.`;
-    } else if (secondsSince < secondsHr) {
-        return `less than hour`;
-    } else if (secondsSince < secondsDay ) { 
-        const hours = Math.floor( secondsSince / secondsHr);
-        if (hours < 2 ) {
-        return `${hours} hour`;
-        } else {
-            return `${hours} hours`;
-        }
-    } else if (secondsSince < secondsMonth) { 
-        const days = Math.floor( secondsSince / secondsDay);
-        if (days === 1) return `1 day`;
-        return `${days} days`;
-    }   else if (secondsSince < secondsYear) { 
-        const months = Math.floor( secondsSince / secondsMonth);
-        return `${months} month`;
-    } else {
-        const years = Math.floor( secondsSince / secondsYear);
-        return `${years} yr.`;
+  if (secondsSince < secondsMin) {
+    return 'less than minute.';
+  } else if (secondsSince < secondsHr) {
+    return 'less than hour';
+  } else if (secondsSince < secondsDay) {
+    const hours = Math.floor(secondsSince / secondsHr);
+    if (hours < 2) {
+      return `${hours} hour`;
     }
-}
+    return `${hours} hours`;
+  } else if (secondsSince < secondsMonth) {
+    const days = Math.floor(secondsSince / secondsDay);
+    if (days === 1) return '1 day';
+    return `${days} days`;
+  } else if (secondsSince < secondsYear) {
+    const months = Math.floor(secondsSince / secondsMonth);
+    return `${months} month`;
+  }
+  const years = Math.floor(secondsSince / secondsYear);
+  return `${years} yr.`;
+};
